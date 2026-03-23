871776b32e7fd5616dea2826e0d5e473f3624d31
Іван Каленич <ivankalenych@example.com>
2026-03-23 20:43:00 +0200

    feat: Added Apartments section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import { Hero } from "../components/home/hero/Hero";
 import { Features } from "../components/home/features/Features";
 import { PopularDistricts } from "../components/home/districtCard/PopularDistricts";
-
+import { ApartmentsSection } from "../components/home/apartament/ApartmentsSection";
 export default function Home() {
   return (
     <main>
       <Hero />
       <Features />
+      <ApartmentsSection />
       <PopularDistricts />
     </main>
   );
